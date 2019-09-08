9c9e3b6dad621ec6f227dcb12c0c9beb2e6278e1
Christian Bager Bach Houmann <chhoumann@gmail.com>
2019-09-08 19:32:46 +0200

    Changed comment format

--- projects/helloTest/helloTest.c
+++ projects/helloTest/helloTest.c
@@ -2,19 +2,21 @@
     This is just a generic 'hello world'-kind-of-program.
 */
 
-#include<stdio.h> // Preprocessor directive. Includes the header file stdio.h (standard input/output), which allows for the printf command below.
-#include<string.h>
+#include <stdio.h> /* Preprocessor directive. Includes the header file stdio.h (standard input/output), which allows for the printf command below. */
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    // Defines str which will hold the name of the user.
-    char str[100];
+    /* Defines str which will hold the name of the user. */
+    char str[10];
 
-    // Prompts the user for their name.
+    /* Prompts the user for their name. */
     printf("What is your name?\nEnter it here: ");
     scanf("%s", str);
 
-    // Prints a given name.
+    /* Prints a given name. */
     printf("Hi, my name is %s.\n", str);
+
     return 0;
 }
