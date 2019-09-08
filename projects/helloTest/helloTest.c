33a5261cc6729b154df7c7f9c86c94a097c426c3
Christian Bager Bach Houmann <chhoumann@gmail.com>
2019-09-08 10:23:57 +0200

    added user prompt and customized greeting.

--- projects/helloTest/helloTest.c
+++ projects/helloTest/helloTest.c
@@ -2,10 +2,19 @@
     This is just a generic 'hello world'-kind-of-program.
 */
 
-#include<stdio.h>
+#include<stdio.h> // Preprocessor directive. Includes the header file stdio.h (standard input/output), which allows for the printf command below.
+#include<string.h>
 
 int main()
 {
-    printf("Hi, my name is Christian.");
+    // Defines str which will hold the name of the user.
+    char str[100];
+
+    // Prompts the user for their name.
+    printf("What is your name?\nEnter it here: ");
+    scanf("%s", str);
+
+    // Prints a given name.
+    printf("Hi, my name is %s.\n", str);
     return 0;
 }
