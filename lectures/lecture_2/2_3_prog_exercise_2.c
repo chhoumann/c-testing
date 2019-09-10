6e14915d707feb9b8fc5e33f69092969e2f2a5be
Christian Bager Bach Houmann <chhoumann@gmail.com>
2019-09-10 20:32:40 +0200

    Lecture 2 prep

---
+++ lectures/lecture_2/2_3_prog_exercise_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void) {
+
+    /* 2.a */
+    int n;
+    printf("The value of n is %d.", n);
+
+    /* 2.b */
+    double side, area;
+    printf("The area of a square whose side length is %f cm is %f square cm", side, area);
+
+}
---
+++ lectures/lecture_2/usingPrintf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* The code below is a part of the lecture video. */
+    int n = 2;
+    double a = 3.5;
+    double b = 1.2;
+
+    printf("Der er %d variable.\na + b = %-6.1f. a - b = %6.1f\n", n, a + b, a - b);
+
+    return 0;
+}
