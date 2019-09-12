236741b645941b72c59a50f5c3e4062a94bda161
Christian Bager Bach Houmann <chhoumann@gmail.com>
2019-09-12 21:24:41 +0200

    quick data types test

---
+++ projects/data_type_tests/data_types_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void) {
+
+    /*  The difference between the different data types is the amount of memory they occupy and the range of values they can hold.  */
+
+    int rgbColor = 0xFFEF0D; /* Ints can store hexadecimals */
+    float sci_not = 1.7e4;
+    double same_as_above_but_bigger = 1.7e4; /* Same type as float but bigger and better */
+    _Bool yes_no = 1;
+
+    
+    return 0;
+}
