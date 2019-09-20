f74163bcaf8b77879c1c2dea38a3ec6389f990b6
Christian Bager Bach Houmann <chhoumann@gmail.com>
2019-09-20 11:18:22 +0200

    finished assignment 2

--- lectures/assignments/assignment_2/timeCalcImprv.c
+++ lectures/assignments/assignment_2/timeCalcImprv.c
@@ -4,12 +4,11 @@
 *   Purpose: Taking an input given in seconds and converting to hours, minutes, and seconds.
 */
 #include <stdio.h>
-#define SECONDS_PER_MNIUTE 60
-#define SECOND_PER_HOUR 3600
+#define SECONDS_PER_MINUTE 60
+#define SECONDS_PER_HOUR 3600
 
 int main(void)
 {
-
     int seconds, minutes, hours;
 
     /* Input */
@@ -17,12 +16,32 @@ int main(void)
     scanf(" %d", &seconds);
 
     /* Calculations */
-    hours = seconds / SECOND_PER_HOUR;
-    minutes = seconds / SECONDS_PER_MNIUTE;
-    seconds = seconds % SECONDS_PER_MNIUTE;
+    hours   = seconds / SECONDS_PER_HOUR;        seconds = seconds - (hours * SECONDS_PER_HOUR);
+    minutes = seconds / SECONDS_PER_MINUTE;      seconds = seconds - (minutes * SECONDS_PER_MINUTE);
+    seconds = seconds % SECONDS_PER_MINUTE;
 
-    /* Output */
-    /* fix */
+    /* Hours output */
+    if (hours > 0){
+        printf("%d %s", hours, (hours > 1) ? "hours" : ((hours == 1) ? "hour" : ""));
+    }
+
+    /* Minutes output */
+    if (minutes > 0){
+        if (hours > 0){
+            printf(", %d %s", minutes, (minutes > 1) ? "minutes" : ((minutes == 1) ? "minute" : ""));
+        } else if (hours == 0) {
+            printf("%d %s", minutes, (minutes > 1) ? "minutes" : ((minutes == 1) ? "minute" : ""));
+        }
+    } 
+
+    /* Seconds output */
+    if (seconds > 0) {
+        if (hours > 0 || minutes > 0) {
+            printf(", %d %s", seconds, (seconds > 1) ? "seconds" : ((seconds == 1) ? "second" : ""));
+        } else if (hours == 0 && minutes == 0){
+            printf("%d %s", seconds, (seconds > 1) ? "seconds" : ((seconds == 1) ? "second" : ""));
+        }
+    }
 
     return 0;
 }
