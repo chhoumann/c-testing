20e8f3143632a125ca1da1701ac5ed04d7259038
Christian Bager Bach Houmann <chhoumann@gmail.com>
2019-10-17 21:40:33 +0200

    finished assignment

--- lectures/assignments/assignment_4/quadEquationSolver.c
+++ lectures/assignments/assignment_4/quadEquationSolver.c
@@ -14,24 +14,48 @@ double solveQuadraticEquation();
 int main() {
     double a, b, c;
 
-    printf("Enter coefficients a, b, and c =>  ");
-    scanf("%lf %lf %lf", &a, &b, &c);
-    solveQuadraticEquation(a, b, c);  
+    /* Repeatedly asks user for an input, until input is 0 0 0.
+       Also makes sure that a != 0. */
+    do{
+        printf("Enter coefficients a, b, and c =>  ");
+        scanf("%lf %lf %lf", &a, &b, &c);
+
+        
+        if (a == 0 && (b != 0 && c != 0))
+            printf("Unable to solve equation - a must be > 0.");
+        else if (a != 0)
+            solveQuadraticEquation(a, b, c); 
+
+        flushInput();
+    } while (a != 0 || b != 0 || c != 0);
 
 }
 
+double solveQuadraticEquation(double a, double b, double c){
+    double root1, root2, discriminant;
+    root2        =  secondRoot(a, b, c);
+    discriminant =  discriminantFinder(a, b, c);
+
+    /* Checks how many roots there are and then prints the roots - if any.
+       Only calls functions if necessary. */
+    if (discriminant < 0)
+        printf("No roots.\n");
+    else if (discriminant == 0){
+        printf("One root: %.2f.\n", firstRoot(a, b, c));
+    } else if (discriminant > 0){
+        printf("Two roots: %.2f and %.2f.\n", firstRoot(a, b, c), secondRoot(a, b, c));
+    }
+}
+
 double discriminantFinder(double a, double b, double c){
-    double discriminant;
-
-    discriminant = b * b - 4 * a * c;
-
-    return discriminant;
+    return (b * b - 4 * a * c);
 }
 
 double firstRoot(double a, double b, double c){
     double root1, discriminant;
     discriminant = discriminantFinder(a, b, c);
 
+    /* Solves for and returns root1. */
     if (discriminant == 0){
         return root1 = -b/(2*a);
     } else {
@@ -44,20 +68,12 @@ double secondRoot(double a, double b, double c){
     double root2, discriminant;
     discriminant = discriminantFinder(a, b, c);
 
+    /* Solves for and returns root2. */
     root2 = (-b - sqrt(discriminant))/(2*a);
     return root2;
 }
 
-double solveQuadraticEquation(double a, double b, double c){
-    double root1, root2, discriminant;
-    root2        =  secondRoot(a, b, c);
-    discriminant =  discriminantFinder(a, b, c);
-
-    if (discriminant < 0)
-        printf("No roots.\n");
-    else if (discriminant == 0){
-        printf("One root: %.2f.\n", firstRoot(a, b, c));
-    } else if (discriminant > 0){
-        printf("Two roots: %.2f and %.2f.\n", firstRoot(a, b, c), secondRoot(a, b, c));
-    }
+void flushInput(void) {
+    int c;
+    while ((c = getchar()) != "\n" && c != EOF);
 }
