d07d084e53a59eb9ce022c58f098ac41698d687c
Christian Bager Bach Houmann <chhoumann@gmail.com>
2019-12-05 20:15:56 +0100

    Finished

--- lectures/assignments/exam_assignment_3/football_tournament.c
+++ lectures/assignments/exam_assignment_3/football_tournament.c
@@ -1,25 +1,7 @@
 /*
 *   Author: Christian Bager Bach Houmann
 *   Due date: 06/12/2019
-*   Purpose: 
-
-fopen fil så den er åbnet til stream.
-Læs enhver linje og tilføj til array af structs (kamp array). Så der skal laves et struct "kamp". 182 kampe.
-    ugedag, dato (uden årstal), klokkeslæt, de to hold, kampens resultat og tilskuertal.
-
-Nu skal der så gennemløbes alle kampresultater for at lave et hold-array hvor et hold er en struct. 14 hold.
-    string holdnavn
-    int point
-        Vundet kamp = 3 point
-        Uafgjort = 1 point
-        Tabt = 0 point
-    int mål-af-hold
-    int mål-mod-hold
-
-Til sidst skal holdene sorteres så man kan se stillingen i turneringen. Der sorteres primært efter point og sekundært efter målforskel.
-    Dette skal udskrives pænt som:
-        Holdnavn Point Mål-af-hold Mål-mod-hold
-
+*   Purpose: Read all football matches from a file and print scores.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -50,21 +32,21 @@ typedef struct {
 } match;
 
 int count_lines_from_file();
-match* match_reader(void);
-team* create_teams(match* matches);
-team * team_sort(match * matches, team * teams, int i, int j, int match_count);
-int team_name_duplicate_checker(char *name, team* teams);
+match * match_reader(void);
+team * create_teams(match* matches);
+team * sort_teams(team * teams);
 
 
 int main(void) {
     int i;
     match* matches = match_reader();
-   
     team* teams = create_teams(matches);
+    teams = sort_teams(teams);
 
-    printf("TEAM NAME | POINTS | GOALS SCORED | GOALS SCORED AGAINST\n");
+    printf("| RANK | TEAM NAME | POINTS | GOALS SCORED | GOALS SCORED AGAINST |\n");
     for (i = 0; i < NUMBER_OF_TEAMS; i++){
-        printf("%s  %d  %d  %d\n", teams[i].team_name, teams[i].points, teams[i].goals_scored, teams[i].goals_against);
+        printf("| %2d#  |  %5s    |  %3d   | %7d      | %11d          |\n", 
+        i+1, teams[i].team_name, teams[i].points, teams[i].goals_scored, teams[i].goals_against);
     }
 
 
@@ -110,6 +92,7 @@ match* match_reader(void) {
     return matches;
 }
 
+/* Index of the teams. Will return where each team would be placed in the teams-array. */
 int h(char *name) {
     if (strcmp(name, "VB") == 0)
         return 0;
@@ -139,10 +122,12 @@ int h(char *name) {
         return 12;
     if (strcmp(name, "BIF") == 0)
         return 13;
+
+    return 0;
 }
 
+/* Puts teams into array of teams and counts stats. */
 team* create_teams(match* matches) {
-    /* Should put team names into array of teams and count stats. */
     int match_count = count_lines_from_file();
     team *teams = malloc(NUMBER_OF_TEAMS * sizeof(team));
     team new_team;
@@ -192,5 +177,28 @@ team* create_teams(match* matches) {
         if (matches[i].home_team_score == matches[i].opponent_team_score)
             teams[index].points += DRAW_POINTS;
     }
+    return teams;
+}
+
+/* Compares scores first and then goal difference. */
+int compare_function(const void * team1, const void * team2){
+    team* temp1 = (team*) team1;
+    team* temp2 = (team*) team2;
+
+    int team1Difference = temp1->goals_scored - temp1->goals_against;
+    int team2Difference = temp2->goals_scored - temp2->goals_against;
+    
+    if (temp1->points > temp2->points)
+        return -1;
+    else if (temp1->points == temp2->points && team1Difference > team2Difference)
+        return -1;
+    else
+        return 1;
+}
+
+/* Sorts the teams using qsort */
+team * sort_teams(team * teams) {
+    qsort(teams, NUMBER_OF_TEAMS, sizeof(team), compare_function);
+
     return teams;
 }
